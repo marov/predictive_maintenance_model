888e4186998b8749d04f16699f205143ba5e138c
marov <mike.arov@postclick.com>
2023-05-08 16:07:51 -0700

    predict.py -> streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+from ml_model.ml_xgboost import XGBClassifierModel
+from predictive_maintenance.modeler import PredictiveMaintananceModeler
 
 """
 # Welcome to Streamlit!
@@ -15,24 +14,15 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+modeler = PredictiveMaintananceModeler()
+modeler.load_data() # in "real life" this would be loading from IoT sensor ETL pipeline
+model = XGBClassifierModel()
+model.load('xgb_classifier_1st_version.pkl')
+inference_date = '2015-06-18 09:00:00'
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Run inference
+labeled_features = modeler.labeled_features
+X_test = pd.get_dummies(labeled_features[labeled_features['datetime'] == inference_date].drop(['datetime', 'machineID', 'comp_to_fail'], axis=1))
+y_pred = model.predict({'X': X_test})['predictions']
+st.write(f"Predicted component to fail: {y_pred[0]}")
+st.write(f"Actual component to fail: {labeled_features[labeled_features['datetime'] == inference_date]['comp_to_fail'].values[0]}")
