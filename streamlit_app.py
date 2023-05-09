14cfc2ec3c83e88b9666eb87136a29ef0c4bf652
marov <mike.arov@postclick.com>
2023-05-08 21:47:29 -0700

    Finish UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import streamlit as st
 from ml_model.ml_xgboost import XGBClassifierModel
 from predictive_maintenance.modeler import PredictiveMaintananceModeler
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Predictive Maintenance :wrench: :factory:
 """
 
 @st.cache_resource
@@ -25,10 +19,41 @@ labeled_features = load_data()
 model = XGBClassifierModel()
 model.load('xgb_classifier_1st_version.pkl')
 
-inference_date = '2015-06-18 09:00:00'
+# inference_date = '2015-06-18 09:00:00' was chosen as default, simly because it had a failure in a component
+
+# Widget to select year, month, day, hour, minute
+date = st.sidebar.date_input('Select date', pd.to_datetime('2015-06-18'),
+                                min_value=labeled_features['datetime'].min(),
+                                max_value=labeled_features['datetime'].max()
+                            )
+time = st.sidebar.time_input('Select time', pd.to_datetime('09:00:00'), step=pd.to_timedelta('3 hour'))
+inference_date = pd.to_datetime(str(date) + ' ' + str(time))
+st.markdown(f"<h3 style='color: blue;'>{inference_date}</h3>", unsafe_allow_html=True)
 
 # Run inference
-X_test = pd.get_dummies(labeled_features[labeled_features['datetime'] == inference_date].drop(['datetime', 'machineID', 'comp_to_fail'], axis=1))
+X_test = pd.get_dummies(labeled_features[labeled_features['datetime'] == inference_date]
+                        .drop(['datetime', 'machineID', 'comp_to_fail'], axis=1))
 y_pred = model.predict({'X': X_test})['predictions']
-st.write(f"Predicted component to fail: {y_pred[0]}")
-st.write(f"Actual component to fail: {labeled_features[labeled_features['datetime'] == inference_date]['comp_to_fail'].values[0]}")
+
+# Display alert of y_pred[0]
+if y_pred[0] == 'none':
+    st.success('No component is predicted to fail')
+else:
+    st.error(f'Component **{y_pred[0][-1]}** is predicted to fail')
+
+# Plot failures in the last 5 days and the next 5 days
+st.subheader(f"Failures in the last 5 days and the next 5 days :chart_with_upwards_trend:")
+points = []
+for i in range(-5, 6):
+    inference_date = pd.to_datetime(str(date) + ' ' + str(time)) + pd.to_timedelta(str(i) + ' day')
+    X_test = pd.get_dummies(labeled_features[labeled_features['datetime'] == inference_date]
+                            .drop(['datetime', 'machineID', 'comp_to_fail'], axis=1))
+    y_pred = model.predict({'X': X_test})['predictions']
+    points.append([inference_date, y_pred[0]])
+points = pd.DataFrame(points, columns=['datetime', 'comp_to_fail'])
+# display the chart with the predictions, mark the current date with a vertical line
+fig, ax = plt.subplots()
+ax.plot(points['datetime'], points["comp_to_fail"], label='Timeline of failures')
+ax.axvline(pd.to_datetime(str(date) + ' ' + str(time)), color='r', linestyle='--', label='Current date')
+ax.legend()
+st.pyplot(fig)
